57fdd559507a0c9795a95dbed53d727a8224ca04
GullianvdWalt <51962834+GullianvdWalt@users.noreply.github.com>
2020-04-15 07:06:46 +0200

    Added home component and added navigation component.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,7 @@
+import { MaterialModule } from './material.module';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FlexLayoutModule } from '@angular/flex-layout'
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
@@ -21,6 +23,11 @@ import { OffersComponent } from './offers/offers.component';
 import { AuthComponent } from './auth/auth.component';
 import { SigninComponent } from './auth/signin/signin.component';
 import { SignupComponent } from './auth/signup/signup.component';
+import { HomeComponent } from './home/home.component';
+import { NavigationComponent } from './navigation/navigation.component';
+import { HeaderComponent } from './navigation/header/header.component';
+import { SidenavListComponent } from './navigation/sidenav-list/sidenav-list.component';
+import { NavtabsComponent } from './navigation/navtabs/navtabs.component';
 
 
 @NgModule({
@@ -43,13 +50,20 @@ import { SignupComponent } from './auth/signup/signup.component';
     OffersComponent,
     AuthComponent,
     SigninComponent,
-    SignupComponent
+    SignupComponent,
+    HomeComponent,
+    NavigationComponent,
+    HeaderComponent,
+    SidenavListComponent,
+    NavtabsComponent
   ],
-  imports: [
+    imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MaterialModule,
+    FlexLayoutModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/material.module.ts
+++ src/app/material.module.ts
@@ -1,14 +1,43 @@
 import { NgModule } from '@angular/core';
-import {MatBadgeModule} from '@angular/material/badge';
+
+import { MatBadgeModule } from '@angular/material/badge';
 import { MatButtonModule } from '@angular/material/button';
-import {Component} from '@angular/core';
-import { MaterialModule } from '@angular/material';
+import { MatIconModule } from '@angular/material/icon';
 import { MatSliderModule } from '@angular/material/slider';
+import { MatRadioModule } from '@angular/material/radio';
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatListModule } from '@angular/material/list';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatTabsModule } from '@angular/material/tabs';
 
 
 @NgModule({
-    imports: [MatBadgeModule, MatButtonModule, MatSliderModule],
-    exports: [MatBadgeModule, MatButtonModule]
+    imports: [
+         MatBadgeModule,
+         MatButtonModule,
+         MatSliderModule,
+         MatIconModule,
+         MatRadioModule,
+         NoopAnimationsModule,
+         MatToolbarModule,
+         MatListModule,
+         MatSidenavModule,
+         MatTabsModule
+    ],
+    exports: [
+        MatBadgeModule,
+        MatButtonModule,
+        MatSliderModule,
+        MatIconModule,
+        MatRadioModule,
+        NoopAnimationsModule,
+        MatToolbarModule,
+        MatListModule,
+        MatSidenavModule,
+        MatTabsModule
+    ]
 })
 
-export class MaterialModule{}
+
+export class MaterialModule {}
